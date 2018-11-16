29393f12f6b7f847e9f94170fb8020ba4b964db7
rebeccaBullTxt <rebeccaBull@sina.com>
2018-11-16 16:58:44 +0800

    20

---
+++ HotUpdate-0.19.0/ios/HotUpdate.framework/Versions/A/Headers/WCDBManager.h
@@ -0,0 +1,19 @@
+//
+//  WCDBManager.h
+//  Pods-HotUpdate_Example
+//
+//  Created by 刘渊 on 2018/11/15.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WCDBManager : NSObject
+/**
+ 单例模式
+ */
++ (instancetype)shared;
+@end
+
+NS_ASSUME_NONNULL_END
